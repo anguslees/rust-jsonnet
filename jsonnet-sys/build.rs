8ea480d34d69e09987528e509c67762e470b3a60
Angus Lees <gus@inodes.org>
2018-05-05 20:52:09 +1000

    Update to libjsonnet v0.10.0

--- jsonnet-sys/build.rs
+++ jsonnet-sys/build.rs
@@ -38,6 +38,7 @@ fn main() {
         "lexer.cpp",
         "libjsonnet.cpp",
         "parser.cpp",
+        "pass.cpp",
         "static_analysis.cpp",
         "string_utils.cpp",
         "vm.cpp",
@@ -46,11 +47,14 @@ fn main() {
     let mut c = gcc::Config::new();
     c.cpp(true)
         .flag("-std=c++0x")
-        .include(dir.join("include"));
+        .include(dir.join("include"))
+        .include(dir.join("third_party/md5"));
 
     for f in &jsonnet_core {
         c.file(dir.join("core").join(f));
     }
+
+    c.file(dir.join("third_party/md5/md5.cpp"));
 
     c.compile("libjsonnet.a")
 }
